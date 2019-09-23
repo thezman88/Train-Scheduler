d249a89189475f418d3552c0269e7008c29438dc
Zachary Cohen <zacharycohen@Zacharys-MBP.home>
2019-09-23 16:36:21 -0400

    firebase added successfully

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,13 @@
-console.log("working");
+$(document).ready(function() {
+      var firebaseConfig = {
+        apiKey: "AIzaSyC-Dsl_wAvKRxN5UdZ9L5urHoNd4l-EXM0",
+        authDomain: "train-schedule-139cf.firebaseapp.com",
+        databaseURL: "https://train-schedule-139cf.firebaseio.com",
+        projectId: "train-schedule-139cf",
+        storageBucket: "",
+        messagingSenderId: "138339544019",
+        appId: "1:138339544019:web:097dc723078c193625dd26"
+      };
+      firebase.initializeApp(firebaseConfig);
+
+    });
