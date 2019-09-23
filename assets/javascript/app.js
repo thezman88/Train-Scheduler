73343195e418b505f4fae59876d738a348c278b6
Zachary Cohen <zacharycohen@Zacharys-MBP.home>
2019-09-23 17:51:02 -0400

    added event listener

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,3 +15,22 @@ $(document).ready(function() {
       var destination;
       var firstTrain;
       var frequency = 0;
+
+      $("#add-train").on("click", function() {
+      event.preventDefault();
+      // Storing and retreiving new train data
+      name = $("#train-name").val().trim();
+      destination = $("#destination").val().trim();
+      firstTrain = $("#first-train").val().trim();
+      frequency = $("#frequency").val().trim();
+
+      // Pushing to database
+      database.ref().push({
+          name: name,
+          destination: destination,
+          firstTrain: firstTrain,
+          frequency: frequency,
+          dateAdded: firebase.database.ServerValue.TIMESTAMP
+      });
+      $("form")[0].reset();
+  });
